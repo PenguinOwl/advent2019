1a89e1d83e4dd9282f214763d9db9f82c6302545
PenguinOwl <henryjboles@gmail.com>
2019-12-12 22:38:36 -0800

    fix normal file

--- 13/post.cr
+++ 13/post.cr
@@ -53,37 +53,6 @@ loop do
 
   Fiber.yield
   intcode.input_channel.send(joy.to_i64) if intcode.waiting
-
-  if last != grid.hash && ball && paddle
-    last = grid.hash
-    bounds = grid.keys.transpose
-    xoffset = bounds[0].min
-    yoffset = bounds[1].min
-    xcap = bounds[0].max
-    ycap = bounds[1].max
-
-    final = Array(Array(Tile)).new(1 + ycap - yoffset) { Array(Tile).new(1 + xcap - xoffset, Tile.new(0)) }
-
-    grid.each do |k, v|
-      final[k[1] - yoffset][k[0] - xoffset] = v
-    end
-
-    string = ""
-
-    final.each do |line|
-      line.each do |tile|
-        if tile == Tile::BALL
-          string += "0"
-        elsif tile != Tile::EMPTY
-          string += "█"
-        else
-          string += " "
-        end
-      end
-      string += "\n"
-    end
-    puts string
-  end
 end
 
 puts score
